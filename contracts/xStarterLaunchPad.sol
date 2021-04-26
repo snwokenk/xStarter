c34269a0f715f81b1b9b3d645513b803b9369093
snwokenkwo <snwokenkwo@terpmail.umd.edu>
2021-04-26 11:35:53 -0400

    working on launchpad

--- contracts/xStarterLaunchPad.sol
+++ contracts/xStarterLaunchPad.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.7.6;
 
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,30 +10,54 @@ import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "./Administration.sol";
-import "./xStarterPoolPair.sol";
+import "./xStarterPoolPairB.sol";
 
 struct ILOProposal {
+    address proposer;
+    string proposalHash; // sha256 hash of proposer + tokenName+ tokenSymbol+ totalSupply(string)+ decimals(string) + percentOfTokensForILO(string)
     string tokenName;
     string tokenSymbol;
     uint totalSupply;
     uint8 decimals;
-    uint8 percentOfTokensForILO;
+    uint8 percentOfTokensForILO; // (minimum 50%)
     address tokenAddress;
     uint blockNumber;
+    bool isApproved;
     
 }
 
 struct GovernanceProposal {
     address proposer;
-    string proposalHash;
+    string proposalHash; // sha256 hash of proposer/title/description
+    uint blockNumber;
+    bool isApproved;
+    
 }
 
+// contract launches xStarterPoolPairB contracts for approved ILO proposals and enforces
 contract xStarterLaunchPad is Ownable{
     using SafeMath for uint256;
     using Address for address;
+    modifier onlyDepositor() {
+        require(depositBalance(_msgSender()) >= minDeposit, 'must have minimum deposit');
+        _;
+    }
     
-    mapping(string => ILOProposal) ILOProposals;
-    mapping(string => GovernanceProposal) govProposals;
+    uint minDeposit;
+    
+    mapping(string => ILOProposal) private ILOProposals;
+    mapping(string => GovernanceProposal) private govProposals;
+    mapping(address => uint) private proposalRefundableDeposits;
+    
+    function depositBalance(address addr_) public view returns(uint) {
+        return proposalRefundableDeposits[addr_];
+    }
+    
+    function createProposal() external onlyDepositor returns(bool success) {
+        
+    }
+    
+    
     
     
 }
