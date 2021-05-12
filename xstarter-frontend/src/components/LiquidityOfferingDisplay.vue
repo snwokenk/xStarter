1c42edd003fe47c33b13c4a877ed12e36b9b677a
snwokenkwo <snwokenkwo@terpmail.umd.edu>
2021-05-11 23:54:18 -0400

    adding ILO display

--- xstarter-frontend/src/components/LiquidityOfferingDisplay.vue
+++ xstarter-frontend/src/components/LiquidityOfferingDisplay.vue
@@ -1,6 +1,6 @@
 <template>
-<q-card>
-  <div class="text-h1">Samuel</div>
+<q-card flat square bordered class="q-py-sm">
+  <div class="display-card-date-text" v-html="startLiveOrEndDisplay" />
 </q-card>
 </template>
 
@@ -16,6 +16,30 @@ export default defineComponent( {
   },
   setup(){
     return {}
+  },
+  computed: {
+    startTimestamp() {
+      return parseInt(this.liquidityOffering.startDate) * 1000
+    },
+    endTimestamp() {
+      return parseInt(this.liquidityOffering.endDate) * 1000
+    },
+    startLiveOrEndDisplay() {
+      const now = Date.now()
+      const endDate = new Date(this.endTimestamp)
+      if (now >= this.startTimestamp && now < this.endTimestamp) {
+        return `Ends on ${endDate}`
+      }else if (now < this.startTimestamp) {
+        return `Starts on  ${new Date(this.startTimestamp)}`
+      }else {
+        `Ended on ${endDate}`
+      }
+      const dateTimestamp =
+      console.log('datetime is', dateTimestamp)
+      let newDate = new Date(dateTimestamp)
+
+      return newDate
+    }
   }
 })
 </script>
--- xstarter-frontend/src/pages/Index.vue
+++ xstarter-frontend/src/pages/Index.vue
@@ -4,8 +4,10 @@
 <!--      alt="Quasar logo"-->
 <!--      src="~assets/quasar-logo-full.svg"-->
 <!--    >-->
-    <div class="display-container">
-      <LiquidityOfferingDisplay class="full-width"  :liquidity-offering="{}"/>
+    <div class="display-container row q-gutter-md justify-center q-pt-md">
+      <LiquidityOfferingDisplay class="display-card col-md-10 col-lg-5"  :liquidity-offering="listOfLiquidityOffering"/>
+      <LiquidityOfferingDisplay class="display-card col-md-10 col-lg-5"  :liquidity-offering="listOfLiquidityOffering"/>
+<!--      <LiquidityOfferingDisplay class="display-card col-md-10 col-lg-5"  :liquidity-offering="listOfLiquidityOffering"/>-->
     </div>
 
   </q-page>
@@ -17,13 +19,24 @@ import LiquidityOfferingDisplay from "components/LiquidityOfferingDisplay";
 
 export default defineComponent({
   name: 'PageIndex',
-  components: {LiquidityOfferingDisplay}
+  components: {LiquidityOfferingDisplay},
+  setup() {
+    return {}
+  },
+  computed: {
+    listOfLiquidityOffering() {
+      return  {
+        startDate: '1620702869',
+        endDate: '1621221269'
+      }
+    }
+  }
 })
 </script>
 <style scoped lang="scss">
 .display-container{
-  width: 50%;
-  min-width: 320px;
-  max-width: 550px;
+  width: 80%;
+  min-height: 300px;
+
 }
 </style>
