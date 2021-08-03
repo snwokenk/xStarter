86d31c0516bab5537b4044ac4210f33ba27fc138
snwokenkwo <snwokenkwo@terpmail.umd.edu>
2021-08-03 15:15:29 -0400

    adding marketplace contract

--- contracts/xStarterMarketPlace.sol
+++ contracts/xStarterMarketPlace.sol
@@ -8,7 +8,6 @@ enum OFFERACTION {
     PAUSE
 }
 struct OfferOption {
-    uint id;
     uint oPrice; // original price
     uint dPrice; // discounted price
     uint oQuantity; // quantity 
@@ -41,6 +40,8 @@ struct Offer {
     uint renewals; 
     bool isDeleted;
     bool isPaused;
+    uint tokenDeposit; // amount to deposit
+    address tokenAddr;
     OfferOption[] options;
     
 }
@@ -51,20 +52,54 @@ contract StorageV1 {
             //countryId     stateId         catId           subCatID
     mapping(uint => mapping(uint => mapping(uint => mapping(uint => Offer[]) ))) offers;
     // [offer][subCatID][catId][stateId][countryId]
+    OfferOption[] offer1;
     
     function getOffer(uint countryId, uint stateId, uint catId, uint subCatID, uint index) public view returns(Offer memory) {
         return offers[countryId][stateId][catId][subCatID][index];
     }
-    function addOffer(uint countryId, uint stateId,  uint cityId, uint catId, uint subCatID, uint expiration, string memory urlInfo, OfferOption[] memory options) public returns(uint index) {
+    
+    function addOffer(uint countryId, uint stateId,  uint cityId, uint catId, uint subCatID, uint expiration, string memory urlInfo,  OfferOption[] memory options) payable public returns(uint index) {
         
-        // todo: have a way of setting the max seconds change from hardcoded
+        // todo: have a way of setting the max seconds change from hardcoded to a changeable variable
+        // todo: have a way of receiving xstn tokens for deposit or Native token
         require(expiration > block.timestamp && (expiration - block.timestamp) > 2592000, 'Listing Greater than 30 days');
-        offers[countryId][stateId][catId][subCatID].push(Offer(msg.sender, urlInfo, countryId, stateId, cityId, catId, subCatID, block.timestamp, block.timestamp + 2500000, 0, false, false, options));
+        offers[countryId][stateId][catId][subCatID].push();
+        Offer storage anOffer = offers[countryId][stateId][catId][subCatID][offers[countryId][stateId][catId][subCatID].length - 1];
+        anOffer.creator = msg.sender;
+        anOffer.urlInfo = urlInfo;
+        anOffer.countryId = countryId;
+        anOffer.stateId = stateId;
+        anOffer.cityId = cityId;
+        anOffer.catId = catId;
+        anOffer.subCatID = subCatID;
+        anOffer.created = block.timestamp;
+        anOffer.exp = expiration ;
+        for (uint i=0; i<options.length; i++) {
+            anOffer.options.push(options[i]);
+        }
         return offers[countryId][stateId][catId][subCatID].length - 1;
     }
+    
     function deleteOrPauseOffer(uint countryId, uint stateId, uint catId, uint subCatID, uint index, OFFERACTION action)  public returns(Offer memory) {
+        Offer storage anOffer = offers[countryId][stateId][catId][subCatID][index];
+        
+        require(anOffer.creator == msg.sender, "not authorized");
+        
+        if(action == OFFERACTION.PAUSE) {
+            anOffer.isPaused = true;
+        }else if (action == OFFERACTION.DELETE) {
+            anOffer.isDeleted = true;
+            uint tmpAmt = anOffer.tokenDeposit;
+            anOffer.tokenDeposit = 0;
+            //  todo: using token add
+        }else {
+            revert("Invalid Action");
+        }
+        
+        return anOffer;
         
     }
+    
     function getSubCatOffers(uint countryId, uint stateId, uint catId, uint subCatID) public view returns(Offer[] memory) {
         return offers[countryId][stateId][catId][subCatID];
     }
@@ -73,8 +108,6 @@ contract StorageV1 {
 }
 
 contract DiscountOffer is Ownable {
-    
-    
     
     
     
